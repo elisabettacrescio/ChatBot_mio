8c881de5c8b793ffa6a60249e0f9c69fa69f439c
elisabettacrescio <elisabetta.crescio@gmail.com>
2025-10-17 07:09:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ app = workflow.compile(checkpointer=memory)
 config = {"configurable": {"user_id": "1", "thread_id": "1"}}
 text_contents = "Tu chat:"
 save_chat = ""
-st.header('Chatbot H1071 ')
+st.header('Chatbot IQ3002C')
 st.write(bot_template.replace("{{MSG}}", "Hola, estoy aquí para ayudarte, ¿Cómo te llamas?"), unsafe_allow_html=True)
 question = st.chat_input("Escribe la pregunta y tu respuesta")
 if question:
